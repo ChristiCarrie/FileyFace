05b55b906ec6e55878f5e4b77e239f365c62dbbd
Aadit Bansal <abansal329@gatech.edu>
2024-09-28 01:11:39 -0400

    moveFile implemented

--- FileChanger.py
+++ FileChanger.py
@@ -1,1 +1,13 @@
 # rename(), move()
+import os
+import math
+
+def moveFile(src, dst):
+    for i in range (math.inf):
+        try:
+            os.rename(src, dst)
+            break
+        except OSError as e:
+            text = os.path.splitext(".")
+            dst = text[0] + '(' + i + ')' + text[1]
+            
